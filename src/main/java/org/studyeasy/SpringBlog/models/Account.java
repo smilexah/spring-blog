308e55a66383b1eda927bf52de24c3ca23e8afcf
smilexah <smilexah@inbox.ru>
2024-02-14 18:12:33 +0600

    Initial commit

---
+++ src/main/java/org/studyeasy/SpringBlog/models/Account.java
@@ -0,0 +1,78 @@
+package org.studyeasy.SpringBlog.models;
+
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Email(message = "Invalid email")
+    @NotEmpty(message = "Email is missing")
+    private String email;
+
+    @NotEmpty(message = "Password is missing")
+    private String password;
+
+    @NotEmpty(message = "First name is missing")
+    private String firstname;
+
+    @NotEmpty(message = "Last name is missing")
+    private String lastname;
+
+    private String gender;
+
+    @Min(value = 18)
+    @Max(value = 65)
+    private int age;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateOfBirth;
+
+    private String photo;
+
+    private String role; 
+
+    @OneToMany(mappedBy = "account")
+    private List<Post> posts;
+
+    @ManyToMany
+    @JoinTable(
+        name="account_authority",
+        joinColumns = {@JoinColumn(name="account_id", referencedColumnName = "id")},
+        inverseJoinColumns = {@JoinColumn(name = "authority_id", referencedColumnName = "id")})
+    private Set<Authority> authorities = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Account [id=" + id + ", email=" + email + ", password=" + password + ", firstname=" + firstname
+                + ", lastname=" + lastname + ", role=" + role + ", posts=" + posts + ", authorities=" + authorities
+                + "]";
+    }
+}
